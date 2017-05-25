1a6e4b634bed1c6090e702e314f758a74edb3e02
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-05-25 21:35:27 +1000

    Create ping.sh

---
+++ ping.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+COUNTER=0
+
+until(curl --fail homes-sitea.test.bxm.net.au/version); do 
+	echo "Pinging Homes Version Page...\n";
+	sleep 5;
+	COUNTER=$(( $COUNTER + 5));
+	echo $COUNTER;
+	if [ $COUNTER == 10 ]; then
+		echo "Could not ping" >&2
+  		exit 1
+	fi
+done
