f5b9fbf27604c832da671b9217b4ab25fe6af107
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-19 22:29:19 +1000

    remove selenium config

--- features/support/chimp.js
+++ features/support/chimp.js
@@ -5,22 +5,5 @@ module.exports = {
                 args: ["--enable-automation", "headless", "disable-gpu"]
             }
         }
-    },
-
-    // - - - - SELENIUM-STANDALONE
-    seleniumStandaloneOptions: {
-        // check for more recent versions of selenium here:
-        // http://selenium-release.storage.googleapis.com/index.html
-        version: '3.0.1',
-        baseURL: 'https://selenium-release.storage.googleapis.com',
-        drivers: {
-            chrome: {
-                // check for more recent versions of chrome driver here:
-                // http://chromedriver.storage.googleapis.com/index.html
-                version: '2.30',
-                arch: process.arch,
-                baseURL: 'https://chromedriver.storage.googleapis.com'
-            }
-        }
     }
 };
