db340acc998c16e82263e2c97e0c7740937e5593
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-19 23:59:00 +1000

    last try

--- features/support/chimp.js
+++ features/support/chimp.js
@@ -2,7 +2,24 @@ module.exports = {
     webdriverio: {
         desiredCapabilities: {
             chromeOptions: {
-                args: ["--enable-automation"]
+                args: ["enable-automation", 'disable-gpu', 'no-sandbox']
+            }
+        }
+    },
+
+    // - - - - SELENIUM-STANDALONE
+    seleniumStandaloneOptions: {
+        // check for more recent versions of selenium here:
+        // http://selenium-release.storage.googleapis.com/index.html
+        version: '3.0.0',
+        baseURL: 'https://selenium-release.storage.googleapis.com',
+        drivers: {
+            chrome: {
+                // check for more recent versions of chrome driver here:
+                // http://chromedriver.storage.googleapis.com/index.html
+                version: '2.29',
+                arch: process.arch,
+                baseURL: 'https://chromedriver.storage.googleapis.com'
             }
         }
     }
