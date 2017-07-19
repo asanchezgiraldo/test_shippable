b889373ebf740656a974ec21ab987eab1ac2ba1c
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-19 11:32:02 +1000

    added xvbf

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable
+apt-get install xvfb
 google-chrome --product-version
