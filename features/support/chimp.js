ca3e7602849a7fb9b16be54203a2c10ee5a9a24f
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-19 23:35:05 +1000

    try with no selenium updates

--- features/support/chimp.js
+++ features/support/chimp.js
@@ -5,22 +5,5 @@ module.exports = {
                 args: ["enable-automation"]
             }
         }
-    },
-
-    // - - - - SELENIUM-STANDALONE
-    seleniumStandaloneOptions: {
-        // check for more recent versions of selenium here:
-        // http://selenium-release.storage.googleapis.com/index.html
-        version: '3.0.1',
-        baseURL: 'https://selenium-release.storage.googleapis.com',
-        drivers: {
-            chrome: {
-                // check for more recent versions of chrome driver here:
-                // http://chromedriver.storage.googleapis.com/index.html
-                version: '2.29',
-                arch: process.arch,
-                baseURL: 'https://chromedriver.storage.googleapis.com'
-            }
-        }
     }
 };
