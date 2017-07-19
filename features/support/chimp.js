afd599baa1a4022b98fb3c3fff58f52e71e6c561
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-19 23:07:05 +1000

    test running normal chrome in xvfb

--- features/support/chimp.js
+++ features/support/chimp.js
@@ -2,7 +2,7 @@ module.exports = {
     webdriverio: {
         desiredCapabilities: {
             chromeOptions: {
-                args: ["--enable-automation", "headless"]
+                args: ["--enable-automation"]
             }
         }
     },
@@ -11,13 +11,13 @@ module.exports = {
     seleniumStandaloneOptions: {
         // check for more recent versions of selenium here:
         // http://selenium-release.storage.googleapis.com/index.html
-        version: '3.0.0',
+        version: '3.0.1',
         baseURL: 'https://selenium-release.storage.googleapis.com',
         drivers: {
             chrome: {
                 // check for more recent versions of chrome driver here:
                 // http://chromedriver.storage.googleapis.com/index.html
-                version: '2.29',
+                version: '2.30',
                 arch: process.arch,
                 baseURL: 'https://chromedriver.storage.googleapis.com'
             }
