794de32fd2fe774d8d834cc32ce16ed2399a6b92
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-20 10:32:20 +1000

    as per lighthouse as it is working for perf test

--- setup.sh
+++ setup.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Add Google Chrome's repo to sources.list
+# as per lighthouse 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
-# Install Google Chrome:
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable
-
+google-chrome --product-version
 
 # Dependencies to make "headless" chrome work:
 sudo apt-get -y install xvfb gtk2-engines-pixbuf
